d960cf7ed2d7c9d1af44905832111bb0b81581ef
yayayagi721 <snoopy1995.1.25@gmail.com>
2023-02-28 00:43:55 +0900

    remove comment

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,10 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // production:  改行やインデントを削除してJSファイル出力
-  // development: ソースマップ有効でJSファイル出力
-  // ただし `webpack --mode production` したときは production が有効になる
   mode: "development",
-
-  // 入力元の指定
   entry: {
     index: path.join(__dirname, "/src/index.ts"),
   },
-
-  // 出力先の指定
   output: {
     path: path.join(__dirname, "dist"),
     filename: "[name].js",
@@ -25,20 +18,18 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/, // 拡張子 .ts のファイルを
-        use: "ts-loader", // ts-loaderでトランスパイルする
-        exclude: /node_modules/, // ただし外部ライブラリは除く
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.(html)$/,
         use: ["html-loader"],
       },
       {
-        test: /\.scss/, // 対象となるファイルの拡張子
+        test: /\.scss/,
         use: [
-          // linkタグに出力する機能
           "style-loader",
-          // CSSをバンドルするための機能
           {
             loader: "css-loader",
             options: {
@@ -63,8 +54,6 @@ module.exports = {
     }),
   ],
 
-  // import './MyLib.ts' が
-  // import './MyLib' の書き方でOKになる
   resolve: {
     extensions: [".ts", ".js"],
   },
